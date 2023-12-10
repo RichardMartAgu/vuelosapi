a87a13786d54bd38a6f120fe10eebba0d64982f5
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2023-12-10 17:46:59 +0100

    Initial commit

---
+++ src/main/java/com/svalero/vuelosapi/VuelosapiApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.vuelosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuelosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuelosapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svalero/vuelosapi/VuelosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.vuelosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuelosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
